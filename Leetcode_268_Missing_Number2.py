030d8824a5899b572284e42c144cce4620f18854
codnegaar <seda1390@gmail.com>
2022-05-31 23:18:46 -0700

    Update Leetcode_268_Missing_Number2.py

--- Leetcode_268_Missing_Number2.py
+++ Leetcode_268_Missing_Number2.py
@@ -10,21 +10,4 @@ class Solution:
             if j not in checked:
                 return j
               
-              
-  ''''
-// Java solution:
-
-class Solution {   
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int  expected = (n*(n+1))/2;
-        int numsSum = 0;
-        for ( int i: nums){
-            numsSum += i;       
-        }        
-         return expected - numsSum ;
-    }
-}
-  
-  
-  .'''
+ 
