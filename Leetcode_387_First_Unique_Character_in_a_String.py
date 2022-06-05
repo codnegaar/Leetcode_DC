229e205be9968622779492b3b44e3663a6d38e6a
codnegaar <seda1390@gmail.com>
2022-06-05 12:35:53 -0700

    Update Leetcode_387_First_Unique_Character_in_a_String.py

--- Leetcode_387_First_Unique_Character_in_a_String.py
+++ Leetcode_387_First_Unique_Character_in_a_String.py
@@ -12,8 +12,9 @@ class Solution:
         A Counter is a dict subclass for counting hashable objects. It is a collection where elements are stored as dictionary
         keys and their counts are stored as dictionary values. Counts are allowed to be any integer value including zero or 
         negative counts. The Counter class is similar to bags or multisets in other languages.
-        
+        Elements are counted from an iterable or initialized from another mapping (or counter):
         '''
+        
         alphabets = collections.Counter(s);
         
         for index in range(len(s)):
