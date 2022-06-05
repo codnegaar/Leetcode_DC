ff55157f5ed0c21cb26d04c3739c940439322dd0
codnegaar <seda1390@gmail.com>
2022-06-05 12:11:19 -0700

    Update Leetcode_387_First_Unique_Character_in_a_String.py

--- Leetcode_387_First_Unique_Character_in_a_String.py
+++ Leetcode_387_First_Unique_Character_in_a_String.py
@@ -2,4 +2,23 @@
 Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 '''
 
-#
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        '''
+        https://docs.python.org/3/library/collections.html    
+        
+        class collections.Counter([iterable-or-mapping])
+        
+        A Counter is a dict subclass for counting hashable objects. It is a collection where elements are stored as dictionary
+        keys and their counts are stored as dictionary values. Counts are allowed to be any integer value including zero or 
+        negative counts. The Counter class is similar to bags or multisets in other languages.
+        
+        '''
+        alphabets = collections.Counter(s);
+        
+        for index in range(len(s)):
+            if alphabets[s[index]] == 1:
+                return index
+        return -1
+      
+      
