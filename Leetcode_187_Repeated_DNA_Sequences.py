84fe904de62035d6be1681193a2ea72b765e3678
codnegaar <seda1390@gmail.com>
2022-06-05 19:26:08 -0700

    Update Leetcode_187_Repeated_DNA_Sequences.py

--- Leetcode_187_Repeated_DNA_Sequences.py
+++ Leetcode_187_Repeated_DNA_Sequences.py
@@ -6,5 +6,21 @@ When studying DNA, it is useful to identify repeated sequences within the DNA.
 
 Given a string s that represents a DNA sequence, return all the 10-letter-long 
 sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
-'''
 
+'''
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        
+        l = len(s)
+        dic = {}
+        DNALST = []
+        
+        for i in range(l):
+            DNA = s[i:i+10]
+            dic [DNA] = dic.get(DNA,0) + 1
+            
+        for key, value in dic.items():
+            if value > 1:
+                DNALST.append(key)
+                
+        return DNALST
