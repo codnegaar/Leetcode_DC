dd9957f077a300d5ec4d6591ee31c11cea9e62ec
Reza <seda1390@gmail.com>
2022-06-10 08:57:18 -0700

    Create Leetcode_56_Merge_Intervals.py

---
+++ Leetcode_56_Merge_Intervals.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key =lambda i: i[0])
+        output = [intervals[0]]
+        for start, end in intervals[1:]:
+            lastIntervalEnd = output[-1][1]
+            if start <= lastIntervalEnd:
+                output[-1][1] = max(lastIntervalEnd , end)
+            else:
+                output.append([start, end])
+        return output
+      
+  '''
+# Second solution 
+if intervals ==[]:
+return []
+sorted = []
+
+intervals.sort() # sort the inrvals
+for intrval in intervals:
+	if sorted == [] or sorted[-1][1] < interval[0]:
+		sorted.append(interval)
+	else: 
+		sorted[-1][1] = max(sorted[-1][1], interval[1])	
+''''	
+	
+
+      
+      
+      
