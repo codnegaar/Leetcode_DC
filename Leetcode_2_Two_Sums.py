54523f5d49cf9ab13d0728072a68ad9d76378eaf
Reza <seda1390@gmail.com>
2022-06-10 17:46:17 -0700

    Create Leetcode_2_Two_Sums.py

---
+++ Leetcode_2_Two_Sums.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexList = {}
+        for i, num in enumerate(nums):
+            delta = target - num
+            if delta in indexList:
+                return [indexList[delta], i]
+            indexList[num] = i
+        return []
