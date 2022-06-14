c99c5c0e6e079351e2a7693d3f4c5130051e9edf
Reza <seda1390@gmail.com>
2022-06-14 00:15:53 -0700

    Update Leetcode_238A_Product_of_Array_Except_Self.py

--- Leetcode_238A_Product_of_Array_Except_Self.py
+++ Leetcode_238A_Product_of_Array_Except_Self.py
@@ -1,1 +1,25 @@
-#
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        pre = [1 for i in range(len(nums))]
+        
+        post = [1 for i in range(len(nums))]
+        
+        
+        for i in range(1, len(nums)):
+            pre[i] = pre[i - 1] * nums[i - 1]
+        
+        
+        for i in range(len(nums) - 2, -1, -1):
+            post[i] = post[i + 1] * nums[i + 1]
+
+            
+        ans = []
+        
+        for p1, p2 in zip(pre, post):
+            ans.append(p1 * p2)
+            
+        return ans 
