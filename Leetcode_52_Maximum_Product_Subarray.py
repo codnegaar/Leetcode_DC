ff3e21528d839d813971522389ca4e9692274583
Reza <seda1390@gmail.com>
2022-06-19 00:05:08 -0700

    Update Leetcode_52_Maximum_Product_Subarray.py

--- Leetcode_52_Maximum_Product_Subarray.py
+++ Leetcode_52_Maximum_Product_Subarray.py
@@ -1,3 +1,11 @@
+'''
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6
+
+'''
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if min(nums)>0:
@@ -17,3 +25,27 @@ class Solution:
             gloMax=max(gloMax, locMax)
         return gloMax
         
+ 
+
+'''
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pos =  nums[0]
+        neg =  nums[0]
+        result = nums[0]
+        
+        for num in nums[1:]:
+            pos = max(num, pos * num, neg * num)
+            neg = min(num, pos * num, neg * num)
+            result = max(result, pos)
+        return result
+
+
+if __name__ == "__main__":
+    assert Solution().maxProduct([2, 3, -2, 4]) == 6
+
+'''
