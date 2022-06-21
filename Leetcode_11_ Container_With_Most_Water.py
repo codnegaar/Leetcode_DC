7c7608ded0117a54ae31a72ab375bfc9678840a1
Reza <seda1390@gmail.com>
2022-06-21 15:00:43 -0700

    Update Leetcode_11_ Container_With_Most_Water.py

--- Leetcode_11_ Container_With_Most_Water.py
+++ Leetcode_11_ Container_With_Most_Water.py
@@ -22,3 +22,15 @@ n == height.length
 '''
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        ans = 0
+        left , right = 0 , len(height)-1
+        
+        while left < right:
+            area = (right-left)*min(height[left], height[right])
+            ans = max(ans, area)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return ans
