1cfcc7d9968baab82db70ecd7b420bfc8d77d0e1
Reza <seda1390@gmail.com>
2022-06-27 11:15:31 -0700

    Update Google_18_Leetcode_112_Path sum.py

--- Google_18_Leetcode_112_Path sum.py
+++ Google_18_Leetcode_112_Path sum.py
@@ -2,6 +2,17 @@
 Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that
 adding up all the values along the path equals targetSum. A leaf is a node with no children.
 
+Solution
+	Runtime complexity: O(N)
+	Space complexity: O(N)
+
+
+This problem can be solved utilizing a simple linear search algorithm, since we already know that inputs are sorted by
+starting timestamps. Here’s the approach we will take:
+List of input intervals is given, and we’ll keep merged intervals in the output list. For each interval,If the input 
+interval is overlapping with the last interval in the output list, we’ll merge the two intervals and update the last 
+interval of the output list with the merged interval. Otherwise, we’ll add an input interval to the output list.
+
 '''
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
