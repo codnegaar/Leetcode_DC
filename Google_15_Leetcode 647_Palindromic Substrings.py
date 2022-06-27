c7e36d079033175085a0ffc389c09f5dfc25978c
Reza <seda1390@gmail.com>
2022-06-26 19:23:20 -0700

    Update Google_15_Leetcode 647_Palindromic Substrings.py

--- Google_15_Leetcode 647_Palindromic Substrings.py
+++ Google_15_Leetcode 647_Palindromic Substrings.py
@@ -26,4 +26,23 @@ Solution:
 
 '''
 
-
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        
+        for i in range(len(s)):
+            res += self.countPalidrom(s, i, i)  # odd index
+            res += self.countPalidrom(s, i, i + 1)   # even index
+        return res
+    
+    def countPalidrom(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            
+            res += 1 # add up to result 
+            
+            l -= 1
+            r += 1
+        return res
+        
+        
