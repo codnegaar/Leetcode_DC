fc2f55a94ae2166998ce3a567025ea5e3c8eb6e9
Reza <seda1390@gmail.com>
2022-12-01 21:28:31 -0800

    Create Leetcode_25_Reverse Nodes in k-Group.py

---
+++ Leetcode_25_Reverse Nodes in k-Group.py
@@ -0,0 +1,64 @@
+
+'''
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+Example 2:
+
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 5000
+0 <= Node.val <= 1000
+
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+  def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    if not head:
+      return None
+
+    tail = head
+
+    for _ in range(k):
+      if not tail:  # Less than k nodes, do nothing
+        return head
+      tail = tail.next
+
+    newHead = self._reverse(head, tail)
+    head.next = self.reverseKGroup(tail, k)
+    return newHead
+
+  # Reverses [head, tail)
+  def _reverse(self, head: Optional[ListNode], tail: Optional[ListNode]) -> Optional[ListNode]:
+    prev = None
+    curr = head
+
+    while curr != tail:
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+
+    return prev
