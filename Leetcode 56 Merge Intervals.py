b7f4494b25e7676577e7c0519db63cfeabfeed1b
Reza <seda1390@gmail.com>
2022-12-23 07:36:29 -0800

    Create Leetcode 56 Merge Intervals.py

---
+++ Leetcode 56 Merge Intervals.py
@@ -0,0 +1,27 @@
+'''
+Leetcode - 56 - Merge Intervals
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the 
+non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+        Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+        Output: [[1,6],[8,10],[15,18]]
+        Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+        Input: intervals = [[1,4],[4,5]]
+        Output: [[1,5]]
+        Explanation: Intervals [1,4] and [4,5] are considered overlapping.  
+Constraints:
+        1 <= intervals.length <= 104
+        intervals[i].length == 2
+        0 <= starti <= endi <= 104
+'''
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+        for interval in sorted(intervals):
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+        return ans
