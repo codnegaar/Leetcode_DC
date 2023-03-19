02a0651b0d18a6274ae43fc3b615c87112e366ae
Reza <seda1390@gmail.com>
2023-03-19 07:19:28 -0700

    Create Leetcode 218 The Skyline Problem.py

---
+++ Leetcode 218 The Skyline Problem.py
@@ -0,0 +1,91 @@
+'''
+218 The Skyline Problem
+
+A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
+
+The geometric information of each building is given in the array buildings where buildings[i] = [lefti, righti, heighti]:
+
+lefti is the x coordinate of the left edge of the ith building.
+righti is the x coordinate of the right edge of the ith building.
+heighti is the height of the ith building.
+You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+
+The skyline should be represented as a list of "key points" sorted by their x-coordinate in the form [[x1,y1],[x2,y2],...]. Each key point is the left endpoint of some horizontal segment in the skyline except the last point in the list, which always has a y-coordinate 0 and is used to mark the skyline's termination where the rightmost building ends. Any ground between the leftmost and rightmost buildings should be part of the skyline's contour.
+
+Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...,[2 3],[4 5],[7 5],[11 5],[12 7],...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...,[2 3],[4 5],[12 7],...]
+
+ 
+
+Example 1:
+
+
+Input: buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
+Output: [[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]
+Explanation:
+Figure A shows the buildings of the input.
+Figure B shows the skyline formed by those buildings. The red points in figure B represent the key points in the output list.
+Example 2:
+
+Input: buildings = [[0,2,3],[2,5,3]]
+Output: [[0,3],[5,0]]
+ 
+
+Constraints:
+
+1 <= buildings.length <= 104
+0 <= lefti < righti <= 231 - 1
+1 <= heighti <= 231 - 1
+buildings is sorted by lefti in non-decreasing order.
+
+'''
+
+class Solution:
+  def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+    n = len(buildings)
+    if n == 0:
+      return []
+    if n == 1:
+      left, right, height = buildings[0]
+      return [[left, height], [right, 0]]
+
+    left = self.getSkyline(buildings[:n // 2])
+    right = self.getSkyline(buildings[n // 2:])
+    return self._merge(left, right)
+
+  def _merge(self, left: List[List[int]], right: List[List[int]]) -> List[List[int]]:
+    ans = []
+    i = 0  # left's index
+    j = 0  # right's index
+    leftY = 0
+    rightY = 0
+
+    while i < len(left) and j < len(right):
+      # Choose the powith smaller x
+      if left[i][0] < right[j][0]:
+        leftY = left[i][1]  # Update the ongoing leftY
+        self._addPoint(ans, left[i][0], max(left[i][1], rightY))
+        i += 1
+      else:
+        rightY = right[j][1]  # Update the ongoing rightY
+        self._addPoint(ans, right[j][0], max(right[j][1], leftY))
+        j += 1
+
+    while i < len(left):
+      self._addPoint(ans, left[i][0], left[i][1])
+      i += 1
+
+    while j < len(right):
+      self._addPoint(ans, right[j][0], right[j][1])
+      j += 1
+
+    return ans
+
+  def _addPoint(self, ans: List[List[int]], x: int, y: int) -> None:
+    if ans and ans[-1][0] == x:
+      ans[-1][1] = y
+      return
+    if ans and ans[-1][1] == y:
+      return
+    ans.append([x, y])
+
+
