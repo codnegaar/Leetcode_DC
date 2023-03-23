c618a982997d1296f7923442d12ffeb7cad9c68a
Reza <seda1390@gmail.com>
2023-03-22 21:23:11 -0700

    Create Leetcode 221 Maximal Square.py

---
+++ Leetcode 221 Maximal Square.py
@@ -0,0 +1,51 @@
+'''
+221 Maximal Square
+ 
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+
+'''
+
+class Solution:
+  def maximalSquare(self, matrix: List[List[chr]]) -> int:
+    m = len(matrix)
+    n = len(matrix[0])
+    dp = [0] * n
+    maxLength = 0
+    prev = 0  # dp[i - 1][j - 1]
+
+    for i in range(m):
+      for j in range(n):
+        cache = dp[j]
+        if i == 0 or j == 0 or matrix[i][j] == '0':
+          dp[j] = 1 if matrix[i][j] == '1' else 0
+        else:
+          dp[j] = min([prev, dp[j], dp[j - 1]]) + 1
+        maxLength = max(maxLength, dp[j])
+        prev = cache
+
+    return maxLength * maxLength
