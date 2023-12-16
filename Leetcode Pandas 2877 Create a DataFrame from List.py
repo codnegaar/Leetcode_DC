570dbecccba5202d87db839bb1c93a06c4b3dd9b
Reza <seda1390@gmail.com>
2023-12-16 07:58:39 -0800

    Update Leetcode Pandas 2877 Create a DataFrame from List.py

--- Leetcode Pandas 2877 Create a DataFrame from List.py
+++ Leetcode Pandas 2877 Create a DataFrame from List.py
@@ -6,8 +6,6 @@ Write a solution to create a DataFrame from a 2D list called student_data. This 
 The DataFrame should have two columns, student_id and age, and be in the same order as the original 2D list.
 The result format is in the following example.
 
- 
-
 Example 1:
 
 Input:
