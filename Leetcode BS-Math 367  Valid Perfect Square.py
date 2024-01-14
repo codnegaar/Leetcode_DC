894a9ee639f57bf6ce03f54218c279b9c8ee2b1f
Reza <seda1390@gmail.com>
2024-01-14 08:47:05 -0800

    Create Leetcode BS-Math 367  Valid Perfect Square.py

---
+++ Leetcode BS-Math 367  Valid Perfect Square.py
@@ -0,0 +1,34 @@
+'''
+Leetcode BS-Math 367  Valid Perfect Square 
+ 
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+You must not use any built-in library function, such as sqrt. 
+
+Example 1:
+          Input: num = 16
+          Output: true
+          Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+          
+Example 2:
+        Input: num = 14
+        Output: false
+        Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+
+Constraints:
+        1 <= num <= 231 - 1
+
+'''
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        low = 1
+        high = num
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == num:
+                return mid
+            elif mid * mid < num:
+                low = mid + 1
+            elif mid * mid > num:
+                high = mid - 1
+        return False
