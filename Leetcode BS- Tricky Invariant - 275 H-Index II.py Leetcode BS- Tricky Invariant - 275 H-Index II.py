96f2ce9116dd7bee278a46737398f35d8a801193
Reza <seda1390@gmail.com>
2024-01-18 06:58:50 -0800

    Update and rename Leetcode 275 H-Index II.py to  Leetcode BS- Tricky Invariant - 275 H-Index II.py Leetcode BS- Tricky Invariant - 275 H-Index II.py

--- Leetcode BS- Tricky Invariant - 275 H-Index II.py Leetcode BS- Tricky Invariant - 275 H-Index II.py
+++ Leetcode BS- Tricky Invariant - 275 H-Index II.py Leetcode BS- Tricky Invariant - 275 H-Index II.py
@@ -1,6 +1,10 @@
 '''
+
+Leetcode BS- Tricky Invariant - 275 H-Index II
+
+
 Given an array of integers citations where citations[i] is the number of citations a researcher received for 
-their ith paper and citations is sorted in ascending order, return the researcher's h-index.
+their ith paper and citations is sorted in ascending order, returning the researcher's h-index.
 According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such 
 that the given researcher has published at least h papers that have each been cited at least h times.
 You must write an algorithm that runs in logarithmic time.
