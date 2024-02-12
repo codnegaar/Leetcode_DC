91535e81bc1c41e4ebf289a8df463e7961e61a7b
Reza <seda1390@gmail.com>
2024-02-12 07:50:48 -0800

    Update and rename Leetcode 62 Unique Paths.py to Leetcode DP-Multidimensional 62 Unique Paths.py

--- Leetcode DP-Multidimensional 62 Unique Paths.py
+++ Leetcode DP-Multidimensional 62 Unique Paths.py
@@ -1,5 +1,5 @@
 '''
-62. Unique Paths
+Leetcode DP-Multidimensional 62 Unique Paths
  
 There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). 
 The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either 
@@ -33,3 +33,9 @@ class Solution:
         dp[j] += dp[j - 1]
 
     return dp[n - 1]
+
+# second solution
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return math.comb(m+n-2, m-1)
+        
