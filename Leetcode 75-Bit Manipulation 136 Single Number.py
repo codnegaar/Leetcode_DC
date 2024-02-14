5f3feb33f47b2f32848d8e24a87d96631d68fb05
Reza <seda1390@gmail.com>
2024-02-14 08:16:51 -0800

    Update and rename Leetcode_136_Single Number.py to Leetcode 75-Bit Manipulation 136 Single Number.py

--- Leetcode 75-Bit Manipulation 136 Single Number.py
+++ Leetcode 75-Bit Manipulation 136 Single Number.py
@@ -1,18 +1,20 @@
-
-
 '''
+
+Leetcode 75-Bit Manipulation 136 Single Number
+
+
 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 You must implement a solution with a linear runtime complexity and use only constant extra space. 
 
-Example 1
-
+Example 1:
         Input: nums = [2,2,1]
         Output: 1
+        
 Example 2:
         Input: nums = [4,1,2,1,2]
         Output: 4
-        Example 3:
 
+Example 3:
       Input: nums = [1]
       Output: 1
 
@@ -21,3 +23,11 @@ Example 2:
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return reduce(lambda x, y: x ^ y, nums, 0)
+
+
+# Second Solution
+
+class Solution:
+        def singleNumber(self, nums: List[int]) -> int:
+            
+            return functools.reduce(operator.xor, nums)
