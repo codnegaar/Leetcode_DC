96ae5ce0b494c34621ee7a31589f688a9df86cbd
Reza <seda1390@gmail.com>
2024-02-28 06:42:45 -0800

    Create Leetcode Tree-  331 Verify Preorder Serialization of a Binary Tree.py

---
+++ Leetcode Tree-  331 Verify Preorder Serialization of a Binary Tree.py
@@ -0,0 +1,51 @@
+'''
+
+331 Verify Preorder Serialization of a Binary Tree
+
+One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
+For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
+Given a string of comma-separated values preorder, return true if it is a correct preorder traversal serialization of a binary tree.
+It is guaranteed that each comma-separated value in the string must be either an integer or a character '#' representing null pointer.
+You may assume that the input format is always valid.
+
+For example, it could never contain two consecutive commas, such as "1,,3".
+Note: You are not allowed to reconstruct the tree.
+
+Example 1:
+        Input: preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#"
+        Output: true
+
+Example 2:
+Input: preorder = "1,#"
+Output: false
+Example 3:
+
+Input: preorder = "9,#,#,1"
+Output: false
+ 
+
+Constraints:
+
+1 <= preorder.length <= 104
+preorder consist of integers in the range [0, 100] and '#' separated by commas ','.
+
+
+'''
+
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        preorder = preorder.split(",")
+        n = len(preorder)
+        if preorder[0] == '#':
+            return n == 1
+        stack = [preorder[0]]
+        
+        for idx in range(1, n):
+            if preorder[idx] != '#':
+                stack.append(preorder[idx])
+            else:
+                if not stack:
+                    return idx == n - 1
+                stack.pop()    
+        return False
+
