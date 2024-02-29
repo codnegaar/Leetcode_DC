028913653089961a603806fefd59990aaff0c479
Reza <seda1390@gmail.com>
2024-02-29 07:09:56 -0800

    Create  Leetcode  Math - 342 Power of Four.py

---
+++ Leetcode  Math - 342 Power of Four.py
@@ -0,0 +1,27 @@
+'''
+Leetcode  Math - 342 Power of Four
+
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+Example 1:
+        Input: n = 16
+        Output: true
+
+Example 2:
+        Input: n = 5
+        Output: false
+
+Example 3:
+        Input: n = 1
+        Output: true 
+
+Constraints:
+        -231 <= n <= 231 - 1
+
+'''
+
+import math
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and math.log2(n) % 2 == 0
