aa05d507c5095312bdd946709182f9d77ddc1d9d
Reza <seda1390@gmail.com>
2024-03-11 07:06:42 -0700

    Update Leetcode  791 Custom Sort String.py

--- Leetcode  791 Custom Sort String.py
+++ Leetcode  791 Custom Sort String.py
@@ -2,8 +2,8 @@
 Leetcode  791 Custom Sort String
 
 You are given two strings order and s. All the characters of the order are unique and were sorted in some custom order previously.
-Permute the characters of s so that they match the order in which the order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
-Return any permutation of s that satisfies this property. 
+Permute the characters of s so that they match the order in which the order was sorted. More specifically, if a character x occurs 
+before a character y in order, then x should occur before y in the permuted string. Return any permutation of s that satisfies this property. 
 
 Example 1:
         Input:  order = "cba", s = "abcd" 
@@ -15,7 +15,8 @@ Example 2:
         Input:  order = "bcafg", s = "abcd" 
         Output:  "bcad" 
         Explanation: The characters "b", "c", and "a" from order dictate the order for the characters in s. The character "d" in s does not appear in order, so its position is flexible.
-        Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", and "a". "d" can be placed at any position since it's not in order. The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", and "a" maintain their order.
+        Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", and "a". "d" can be placed at any position since it's not in order.
+        The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", and "a" maintain their order.
 
 Constraints:
         1 <= order.length <= 26
