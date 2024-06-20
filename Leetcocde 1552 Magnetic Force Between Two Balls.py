d87a444f1703b293e03685c8b498fe8ea590869d
Reza <seda1390@gmail.com>
2024-06-20 07:04:52 -0700

    Create Leetcocde 1552 Magnetic Force Between Two Balls.py

---
+++ Leetcocde 1552 Magnetic Force Between Two Balls.py
@@ -0,0 +1,56 @@
+'''
+
+Leetcocde 1552 Magnetic Force Between Two Balls
+
+In the universe Earth C-137, Rick discovered a special form of magnetic force between two balls if they are put in his new invented basket. Rick has n empty baskets, the ith basket is at position[i], 
+Morty has m balls and needs to distribute the balls into the baskets such that the minimum magnetic force between any two balls is maximum.
+
+Rick stated that magnetic force between two different balls at positions x and y is |x - y|.
+Given the integer array position and the integer m. Return the required force.
+
+
+Example 1:
+        Input: position = [1,2,3,4,7], m = 3
+        Output: 3
+        Explanation: Distributing the 3 balls into baskets 1, 4 and 7 will make the magnetic force between ball pairs [3, 3, 6]. The minimum magnetic force is 3. We cannot achieve a larger minimum magnetic force than 3.
+
+Example 2:        
+        Input: position = [5,4,3,2,1,1000000000], m = 2
+        Output: 999999999
+        Explanation: We can use baskets 1 and 1000000000.
+         
+
+Constraints:
+
+n == position.length
+2 <= n <= 105
+1 <= position[i] <= 109
+All integers in position are distinct.
+2 <= m <= position.length
+
+
+'''
+
+class Solution:
+    def fn(self, p, m, mid):
+        cnt = 1
+        prev = p[0]
+        for i in range(1, len(p)):
+            if p[i] - prev >= mid:
+                cnt += 1
+                prev = p[i]
+        return cnt >= m
+
+    def maxDistance(self, p, m):
+        p.sort()
+        lo = 1
+        hi = p[-1] - p[0]
+        while hi - lo > 1:
+            mid = (lo + hi) // 2
+            if self.fn(p, m, mid):
+                lo = mid
+            else:
+                hi = mid - 1
+        if self.fn(p, m, hi):
+            return hi
+        return lo
