f53e97185fb7e5dbc692e160b8b573d96bb22934
Reza <seda1390@gmail.com>
2024-08-29 07:38:00 -0700

    Update Leetcode 947 Most Stones Removed with Same Row or Column.py

--- Leetcode 947 Most Stones Removed with Same Row or Column.py
+++ Leetcode 947 Most Stones Removed with Same Row or Column.py
@@ -38,3 +38,40 @@ Constraints:
 
 ''''
 
+from typing import List
+
+class Solution:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+        self.connected_components = 0
+
+    def removeStones(self, stones: List[List[int]]) -> int:
+        for x, y in stones:
+            self.union(x, ~y)  # Use bitwise NOT to distinguish between x and y coordinates.
+        return len(stones) - self.connected_components
+
+    def find(self, x: int) -> int:
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+            self.connected_components += 1
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])  # Path compression.
+        return self.parent[x]
+
+    def union(self, x: int, y: int) -> None:
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+            self.connected_components -= 1
+
+
