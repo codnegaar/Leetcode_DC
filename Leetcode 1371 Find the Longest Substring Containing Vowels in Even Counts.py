84bc7a50665dd60cddff8d835e1b5f2255de1f60
Reza <seda1390@gmail.com>
2024-09-15 08:16:22 -0700

    Create Leetcode 1371 Find the Longest Substring Containing Vowels in Even Counts.py

---
+++ Leetcode 1371 Find the Longest Substring Containing Vowels in Even Counts.py
@@ -0,0 +1,41 @@
+'''
+
+Leetcode 1371 Find the Longest Substring Containing Vowels in Even Counts
+
+Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
+ 
+Example 1:
+        Input: s = "eleetminicoworoep"
+        Output: 13
+        Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
+        
+Example 2:
+        Input: s = "leetcodeisgreat"
+        Output: 5
+        Explanation: The longest substring is "leetc" which contains two e's.
+        
+Example 3:
+        Input: s = "bcbcbc"
+        Output: 6
+        Explanation: In this case, the given string "bcbcbc" is the longest because all vowels: a, e, i, o and u appear zero times.
+ 
+Constraints:
+        1 <= s.length <= 5 x 10^5
+        s contains only lowercase English letters.
+
+'''
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        vowels = {'a': 1, 'e': 2, 'i': 4, 'o': 8, 'u': 16}
+        state_map = {0: -1}
+        max_len, mask = 0, 0
+
+        for i, char in enumerate(s):
+            if char in vowels:
+                mask ^= vowels[char]
+            if mask in state_map:
+                max_len = max(max_len, i - state_map[mask])
+            else:
+                state_map[mask] = i
+
+        return max_len
