f4f049da0d24a825bedc6831962e7a536b15e130
Reza <seda1390@gmail.com>
2024-12-28 09:04:52 -0800

    Update Leetcode 689 Maximum Sum of 3 Non-Overlapping Subarrays.py

--- Leetcode 689 Maximum Sum of 3 Non-Overlapping Subarrays.py
+++ Leetcode 689 Maximum Sum of 3 Non-Overlapping Subarrays.py
@@ -1,5 +1,5 @@
 '''
-689 Maximum Sum of 3 Non-Overlapping Subarrays
+Leetcode 689 Maximum Sum of 3 Non-Overlapping Subarrays
  
 Given an integer array nums and an integer k, find three non-overlapping subarrays of length k with maximum sum and return them.
 
