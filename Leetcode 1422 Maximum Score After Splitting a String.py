b3e3efbd81333e4d7fd30331c4a7a7bcc46d4651
Reza <seda1390@gmail.com>
2025-01-01 09:16:47 -0800

    Update Leetcode 1422 Maximum Score After Splitting a String.py

--- Leetcode 1422 Maximum Score After Splitting a String.py
+++ Leetcode 1422 Maximum Score After Splitting a String.py
@@ -1,5 +1,4 @@
 '''
-
 Leetcode 1422 Maximum Score After Splitting a String
  
 Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
