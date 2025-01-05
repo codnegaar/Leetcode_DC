99ee3bc93efc60f2f4db8ff4f22eea9ca982830b
Reza <seda1390@gmail.com>
2025-01-04 20:27:13 -0800

    Update and rename 115 Distinct Subsequences.py to Leetcode 115 Distinct Subsequences.py

--- Leetcode 115 Distinct Subsequences.py
+++ Leetcode 115 Distinct Subsequences.py
@@ -1,10 +1,9 @@
 '''
 
-115 Distinct Subsequences
+Leetcode 115 Distinct Subsequences
  
 Given two strings s and t, return the number of distinct 
 subsequences of s which equals t.
-
 The test cases are generated so that the answer fits on a 32-bit signed integer. 
 
 Example 1:
