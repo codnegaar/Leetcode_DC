c0ae5005956ebe74d4a973b1e8a0ef27463372dd
Reza <seda1390@gmail.com>
2025-01-15 06:49:28 -0800

    Update 564 Find the Closest Palindrome.py

--- 564 Find the Closest Palindrome.py
+++ 564 Find the Closest Palindrome.py
@@ -1,5 +1,5 @@
 '''
-564 Find the Closest Palindrome
+Leetcode 564 Find the Closest Palindrome
 
 Given a string n representing an integer, return the closest integer (not including itself), which is a palindrome. If there is a tie, return the smaller one.
 The closest is defined as the absolute difference minimized between two integers. 
