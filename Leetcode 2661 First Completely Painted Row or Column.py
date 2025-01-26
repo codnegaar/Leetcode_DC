74ea1567bb1aa9ddb3583f51b1bc25026081920f
Reza <seda1390@gmail.com>
2025-01-26 14:54:38 -0800

    Update and rename 2661 First Completely Painted Row or Column.py to Leetcode 2661 First Completely Painted Row or Column.py

--- Leetcode 2661 First Completely Painted Row or Column.py
+++ Leetcode 2661 First Completely Painted Row or Column.py
@@ -1,5 +1,5 @@
 '''
-2661 First Completely Painted Row or Column
+Leetcode 2661 First Completely Painted Row or Column
 
 You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
 Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i].
