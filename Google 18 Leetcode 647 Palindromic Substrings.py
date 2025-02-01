3688effd667c42b351f7123ec6ce94e3f875f9ee
Reza <seda1390@gmail.com>
2025-02-01 07:43:13 -0800

    Update and rename Google_18_Leetcode 647_Palindromic Substrings.py to Google 18 Leetcode 647 Palindromic Substrings.py

--- Google 18 Leetcode 647 Palindromic Substrings.py
+++ Google 18 Leetcode 647 Palindromic Substrings.py
@@ -1,4 +1,5 @@
 '''
+Google 18 Leetcode 647 Palindromic Substrings
 
 Given a string s, return the number of palindromic substrings in it. A string is a palindrome when it reads the same backward as forward.
 A substring is a contiguous sequence of characters within the string.
