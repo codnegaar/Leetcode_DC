8cbbf53016e3ee1d9f6facb27bfd297ee8c717b4
Reza <seda1390@gmail.com>
2025-02-14 15:39:11 -0800

    Update and rename 1352 Product of the Last K Numbers.py to Leetcode 1352 Product of the Last K Numbers.py

--- Leetcode 1352 Product of the Last K Numbers.py
+++ Leetcode 1352 Product of the Last K Numbers.py
@@ -1,5 +1,5 @@
 '''
-1352 Product of the Last K Numbers
+Leetcode 1352 Product of the Last K Numbers
   
 Design an algorithm that accepts a stream of integers and retrieves the product of the last k integers of the stream.
 Implement the ProductOfNumbers class:
