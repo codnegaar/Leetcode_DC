89f7861343cc2f782e1677ca293bb22ee88b872a
Reza <seda1390@gmail.com>
2025-03-31 17:44:24 -0700

    Create Leetcode 2140 Solving Questions With Brainpower.py

---
+++ Leetcode 2140 Solving Questions With Brainpower.py
@@ -0,0 +1,70 @@
+'''
+Leetcode 2140 Solving Questions With Brainpower
+
+You are given a 0-indexed 2D integer array questions where questions[i] = [pointsi, brainpoweri].
+The array describes the questions of an exam, where you have to process the questions in order (i.e., starting from question 0) and make a decision whether
+to solve or skip each question. Solving question i will earn you pointsi points but you will be unable to solve each of the next brainpoweri questions. 
+If you skip question i, you get to make the decision on the next question.
+
+For example, given questions = [[3, 2], [4, 3], [4, 4], [2, 5]]:
+If question 0 is solved, you will earn 3 points but you will be unable to solve questions 1 and 2.
+If instead, question 0 is skipped and question 1 is solved, you will earn 4 points but you will be unable to solve questions 2 and 3.
+Return the maximum points you can earn for the exam.
+
+Example 1:        
+        Input: questions = [[3,2],[4,3],[4,4],[2,5]]
+        Output: 5
+        Explanation: The maximum points can be earned by solving questions 0 and 3.
+        - Solve question 0: Earn 3 points, will be unable to solve the next 2 questions
+        - Unable to solve questions 1 and 2
+        - Solve question 3: Earn 2 points
+        Total points earned: 3 + 2 = 5. There is no other way to earn 5 or more points.
+
+Example 2:
+        Input: questions = [[1,1],[2,2],[3,3],[4,4],[5,5]]
+        Output: 7
+        Explanation: The maximum points can be earned by solving questions 1 and 4.
+        - Skip question 0
+        - Solve question 1: Earn 2 points, will be unable to solve the next 2 questions
+        - Unable to solve questions 2 and 3
+        - Solve question 4: Earn 5 points
+        Total points earned: 2 + 5 = 7. There is no other way to earn 7 or more points.
+         
+Constraints:
+        1 <= questions.length <= 105
+        questions[i].length == 2
+        1 <= pointsi, brainpoweri <= 105
+'''
+from typing import List
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        """
+        Calculates the maximum points one can earn by solving non-overlapping questions.
+        Each question has a 'brainpower' cooldown that skips the next few questions.
+
+        Parameters:
+        questions (List[List[int]]): A list where each element is [points, brainpower]
+
+        Returns:
+        int: Maximum points achievable.
+        """
+        n = len(questions)
+        # dp[i] represents the max points obtainable starting from question i
+        dp = [0] * (n + 1)  # Extra space to avoid index out-of-range
+
+        # Process from the last question to the first
+        for i in reversed(range(n)):
+            points, brainpower = questions[i]
+            next_index = i + brainpower + 1
+
+            # If we solve this question, add points and skip brainpower questions
+            solve = points + (dp[next_index] if next_index < n else 0)
+
+            # If we skip this question, take dp[i + 1]
+            skip = dp[i + 1]
+
+            # Take the maximum of solving or skipping
+            dp[i] = max(solve, skip)
+
+        return dp[0]
