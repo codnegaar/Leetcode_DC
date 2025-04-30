febaef51df82211f3d5c94bd157e9358b2e51008
Reza <seda1390@gmail.com>
2025-04-29 17:51:40 -0700

    Update and rename Leetcode_5_Longest Palindromic Substring.py to Leetcode 5 Longest Palindromic Substring.py

--- Leetcode 5 Longest Palindromic Substring.py
+++ Leetcode 5 Longest Palindromic Substring.py
@@ -1,4 +1,5 @@
 '''
+Leetcode 5 Longest Palindromic Substring
 
 Given a string s, return the longest palindromic substring in s.
 A string is called a palindrome string if the reverse of that string is the same as the original string. 
