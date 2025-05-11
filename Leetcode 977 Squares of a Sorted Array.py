8d9f90cc98aeddb763948cae230025c04ac06df9
Reza <seda1390@gmail.com>
2025-05-10 18:40:09 -0700

    Update and rename Leetcode_977_Squares of a Sorted Array.py to Leetcode 977 Squares of a Sorted Array.py

--- Leetcode 977 Squares of a Sorted Array.py
+++ Leetcode 977 Squares of a Sorted Array.py
@@ -1,4 +1,5 @@
 '''
+Leetcode 977 Squares of a Sorted Array.py
 
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
