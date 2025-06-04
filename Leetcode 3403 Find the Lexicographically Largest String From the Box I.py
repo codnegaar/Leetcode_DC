4e5b736785c48556cd8f5e4442e532af66ff8e24
Reza <seda1390@gmail.com>
2025-06-03 17:59:35 -0700

    Create Leetcode 3403 Find the Lexicographically Largest String From the Box I.py

---
+++ Leetcode 3403 Find the Lexicographically Largest String From the Box I.py
@@ -0,0 +1,37 @@
+'''
+Leetcode 3403 Find the Lexicographically Largest String From the Box I
+
+You are given a string word, and an integer numFriends.
+Alice is organizing a game for her numFriends friends. There are multiple rounds in the game, where in each round:
+word is split into numFriends non-empty strings, such that no previous round has had the exact same split.
+All the split words are put into a box.
+Find the lexicographically largest string from the box after all the rounds are finished.
+ 
+Example 1:
+        Input: word = "dbca", numFriends = 2
+        Output: "dbc"        
+        Explanation: All possible splits are:
+        
+        "d" and "bca".
+        "db" and "ca".
+        "dbc" and "a".
+
+Example 2:
+        Input: word = "gggg", numFriends = 4
+        Output: "g"
+        Explanation: The only possible split is: "g", "g", "g", and "g".
+
+Constraints:
+        1 <= word.length <= 5 * 103
+        word consists only of lowercase English letters.
+        1 <= numFriends <= word.length
+
+'''
+
+class Solution:
+    def answerString(self, word: str, n: int) -> str:
+        m=len(word)-n+1 # splits to be done with m length 
+        if n==1: # if only one split we can do total word is taken 
+            return word
+# else check all m length splits and gind max in them 
+        return max(word[i:i+m] for i in range(len(word)))
