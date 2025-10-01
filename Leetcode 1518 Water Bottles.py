47ccb2685aea016751b6b5bbe965358f07b449d5
Reza <seda1390@gmail.com>
2025-09-30 17:03:55 -0700

    Update Leetcode 1518 Water Bottles.py

--- Leetcode 1518 Water Bottles.py
+++ Leetcode 1518 Water Bottles.py
@@ -1,5 +1,4 @@
 '''
-
 Leetcode 1518 Water Bottles
 
 There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
