d2e0a9838b606266186a48557f0794106abe20a7
Reza <seda1390@gmail.com>
2025-12-07 16:22:12 -0800

    Create Leetcode 1925 Count Square Sum Triples.py

---
+++ Leetcode 1925 Count Square Sum Triples.py
@@ -0,0 +1,24 @@
+'''
+Leetcode 1925 Count Square Sum Triples
+ 
+A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+Given an integer n, return the number of square triples such that 1 <= a, b, c <= n. 
+
+Example 1:
+        Input: n = 5
+        Output: 2
+        Explanation: The square triples are (3,4,5) and (4,3,5).
+
+Example 2:
+        Input: n = 10
+        Output: 4
+        Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ 
+Constraints:
+        1 <= n <= 250
+'''
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        return sum(isqrt(q:=a*a+b*b)**2==q<=n*n
+            for a,b in combinations(range(1,n+1),2))*2
