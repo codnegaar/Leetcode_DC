1c610b4b7fd6af47369ede606f2277ee5ffaceb7
Reza <seda1390@gmail.com>
2026-07-24 18:57:10 -0700

    Refactor examples in Maximum Product of Two Digits
    Updated example inputs and outputs for clarity.

---
+++ Leetcode 3536 Maximum Product of Two Digits.py
@@ -0,0 +1,49 @@
+'''
+
+Leetcode 3536 Maximum Product of Two Digits
+ 
+You are given a positive integer n.
+Return the maximum product of any two digits in n.
+Note: You may use the same digit twice if it appears more than once in n. 
+
+Example 1:
+        Input: n = 31
+        Output: 3
+        Explanation:
+        The digits of n are [3, 1].
+        The possible products of any two digits are: 3 * 1 = 3.
+        The maximum product is 3.
+
+Example 2:        
+        Input: n = 22        
+        Output: 4        
+        Explanation:        
+        The digits of n are [2, 2].
+        The possible products of any two digits are: 2 * 2 = 4.
+        The maximum product is 4.
+
+Example 3:
+        Input: n = 124
+        Output: 8
+        Explanation:
+        The digits of n are [1, 2, 4].
+        The possible products of any two digits are: 1 * 2 = 2, 1 * 4 = 4, 2 * 4 = 8.
+        The maximum product is 8.
+ 
+Constraints:
+        10 <= n <= 109
+'''
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        max1 = max2 = 0
+
+        while n:
+            d = n % 10
+            if d >= max1:
+                max2 = max1
+                max1 = d
+            elif d > max2:
+                max2 = d
+            n //= 10
+
+        return max1 * max2
