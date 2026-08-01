78137c3b519de111be5ed7943f8e16332413178f
Reza <seda1390@gmail.com>
2026-07-31 17:53:47 -0700

    Add game rules docstring for Predict the Winner
    Added a docstring explaining the game rules for Leetcode 486.

--- Algorithm-DS-DP/Leetcode  486 Predict the Winner.py
+++ Algorithm-DS-DP/Leetcode  486 Predict the Winner.py
@@ -2,7 +2,6 @@
 Leetcode 486 Predict the Winner
 
 You are given an integer array nums. Two players are playing a game with this array: player 1 and player 2.
-
 Player 1 and Player 2 take turns, with Player 1 starting first. Both players start the game with a score of 0. At each turn, 
 the player takes one of the numbers from either end of the array (i.e., nums[0] or nums[nums.length - 1]) which reduces the size 
 f the array by 1. The player adds the chosen number to their score. The game ends when there are no more elements in the array.
