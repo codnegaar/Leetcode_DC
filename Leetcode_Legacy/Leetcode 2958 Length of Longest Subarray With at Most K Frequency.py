ba6b6f5592d227c5ca073783cd96921d5f96cde8
Reza <seda1390@gmail.com>
2026-08-11 17:13:26 -0700

    Update docstring for Leetcode 2958 problem

--- Leetcode_Legacy/Leetcode 2958 Length of Longest Subarray With at Most K Frequency.py
+++ Leetcode_Legacy/Leetcode 2958 Length of Longest Subarray With at Most K Frequency.py
@@ -1,6 +1,6 @@
 ﻿'''
+ 
 Leetcode 2958 Length of Longest Subarray With at Most K Frequency
-
  
 You are given an integer array nums and an integer k.
 The frequency of an element x is the number of times it occurs in an array.
